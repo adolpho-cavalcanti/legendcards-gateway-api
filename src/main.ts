51d3e14828d6c1bfd60982902f1ce7588deb1150
elninocavalcanti10 <acn2011@outlook.com>
2022-03-14 20:51:02 -0300

    Add env variables

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/http.exception.filter';
@@ -8,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor());
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8080;
   await app.listen(port);
 }
 bootstrap();
--- src/proxyrmq/client-proxy.ts
+++ src/proxyrmq/client-proxy.ts
@@ -1,13 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { ClientProxy, ClientProxyFactory, Transport } from "@nestjs/microservices";
 
+const user = process.env.RABBITMQ_USER;
+const pass = process.env.RABBITMQ_PASS;
+const console = process.env.RABBITMQ_CONSOLE;
+
 @Injectable()
 export class ClientProxyMicroLegend {
     getClientProxyMicroLegend(): ClientProxy {
         return ClientProxyFactory.create({
             transport: Transport.RMQ,
             options: {
-              urls: ['amqp://user:Ti5Ls2ahgaJ0@3.89.120.123:5672/legendsCards'],
+              urls: [`amqp://${user}:${pass}@3.89.120.123:5672/${console}`],
               queue: 'micro-legend'
             }
         })
